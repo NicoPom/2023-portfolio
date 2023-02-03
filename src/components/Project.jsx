8ecdcdeddfcd697241f75ae268791742d0872c2e
Nico Pom <niconp7@yahoo.fr>
2023-02-03 14:59:23 +0100

    cleaned up project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,21 +6,26 @@ import "../styles/project.css";
 const Project = ({
   project: { id, title, thumbnail, alt, stack, details, liveSite },
 }) => {
+  // Get the "isInfoOpen" value from the context
   const { isInfoOpen } = useContext(ProjectContext);
+
+  // State to store the appropriate image source
   const [src, setSrc] = useState(thumbnail.mobile);
 
-  const handleResize = () => {
-    // handle the image source depending on the screen size
-    window.innerWidth > 1000
-      ? setSrc(thumbnail.desktop)
-      : window.innerWidth > 700
-      ? setSrc(thumbnail.tablet)
-      : setSrc(thumbnail.mobile);
-  };
-
+  // Update the image source based on the screen size
   useEffect(() => {
+    const handleResize = () => {
+      window.innerWidth > 1000
+        ? setSrc(thumbnail.desktop)
+        : window.innerWidth > 700
+        ? setSrc(thumbnail.tablet)
+        : setSrc(thumbnail.mobile);
+    };
+
     window.addEventListener("resize", handleResize);
     handleResize();
+
+    // Clean up the event listener
     return () => {
       window.removeEventListener("resize", handleResize);
     };
@@ -32,7 +37,6 @@ const Project = ({
       <a href={liveSite} target="_blank" rel="noopener noreferrer">
         <img className="raise" src={src} alt={alt} />
       </a>
-
       <Infos stack={stack} details={details} isInfoOpen={isInfoOpen[id]} />
     </div>
   );
