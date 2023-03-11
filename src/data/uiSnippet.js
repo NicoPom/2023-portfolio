ce0cef4b9b6427226221d55cf37353d1d21f0471
Nico Pom <niconp7@yahoo.fr>
2023-03-11 16:44:14 +0100

    added cursor trail project

--- src/data/uiSnippet.js
+++ src/data/uiSnippet.js
@@ -17,6 +17,15 @@ const uiSnippetsData = [
     details: `This is a smooth switch that I created to practice my CSS and GSAP skills.`,
     stack: "HTML5 | CSS3 | JS | GSAP | Netlify",
   },
+  {
+    id: "cursor trail",
+    title: "cursor trail",
+    iframe: "https://cursor-trail.netlify.app/",
+    liveSite: "https://cursor-trail.netlify.app/",
+    github: "https://github.com/NicoPom/cursor-trail",
+    details: `This is a cursor trail that I created to practice my and GSAP skills.`,
+    stack: "HTML5 | CSS3 | JS | GSAP | Netlify",
+  },
 ];
 
 export { uiSnippetsData };
